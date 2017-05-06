65b7bdac5752790c1c185f714997659b81d2eb3a
Peter Zhu <peterzhu2118@gmail.com>
2017-05-06 12:56:18 -0400

    Add merge sort with insertion

---
+++ src/test/java/ca/peterzhu/algorithms/MergeSortWithInsertionTest.java
@@ -0,0 +1,18 @@
+package ca.peterzhu.algorithms;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class MergeSortWithInsertionTest {
+	
+	@Test
+	public void testSort() {
+		Assert.assertArrayEquals(
+				new int[] { -15, -10, -10, 0, 1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 6,
+						6, 7, 7, 8, 10 },
+				MergeSortWithInsertion.sort(new int[] { 5, 10, 7, 6, 3, 2, 1, 1, 2, 5, 6, 7,
+						8, 3, 0, 1, 4, -10, -15, -10 }));
+	}
+
+}
